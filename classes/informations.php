7f6ecf0c91eac0f733693929815d5e5dc6b2fe83
carminelentisco <carminelentisco@gmail.com>
2020-06-08 18:34:54 +0200

    Info Corretta

--- classes/informations.php
+++ classes/informations.php
@@ -4,7 +4,7 @@ include_once __DIR__ . '/Dipendente.php';
 try {
 
     $informations = [
-        new Dipendente('IBIS', rand(100, 200), 'Carmine' , 'Lentisco', '20', 'Salumificio', 'taglio'),
+        new Dipendente('IBIS', rand(100, 200), 'Carmine' , 'Lentisco', 20, 'Salumificio', 'taglio'),
         new Dipendente('IBIS', rand(100, 200), 'Franco' , 'Cozzo', 18, 'Panificio', 'Forni'),
         new Dipendente('IBIS', rand(100, 200), 'Ettore' , 'Gianfranco', 27, 'Salumificio', 'Culatelli'),
         new Dipendente('IBIS', rand(100, 200), 'Carmine' , 'Lentisco', 20, 'Salumificio', 'taglio'),
