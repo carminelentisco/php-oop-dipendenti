26ffdc0d2104add5a9a94781559d7db91984ff94
carminelentisco <carminelentisco@gmail.com>
2020-06-08 18:32:40 +0200

    Aggiunti dipendenti

--- classes/informations.php
+++ classes/informations.php
@@ -4,9 +4,15 @@ include_once __DIR__ . '/Dipendente.php';
 try {
 
     $informations = [
+        new Dipendente('IBIS', rand(100, 200), 'Carmine' , 'Lentisco', '20', 'Salumificio', 'taglio'),
+        new Dipendente('IBIS', rand(100, 200), 'Franco' , 'Cozzo', 18, 'Panificio', 'Forni'),
+        new Dipendente('IBIS', rand(100, 200), 'Ettore' , 'Gianfranco', 27, 'Salumificio', 'Culatelli'),
         new Dipendente('IBIS', rand(100, 200), 'Carmine' , 'Lentisco', 20, 'Salumificio', 'taglio'),
         new Dipendente('IBIS', rand(100, 200), 'Franco' , 'Cozzo', 18, 'Panificio', 'Forni'),
         new Dipendente('IBIS', rand(100, 200), 'Ettore' , 'Gianfranco', 27, 'Salumificio', 'Culatelli'),
+        new Dipendente('IBIS', rand(100, 200), 'Ettore' , 'Gianfranco', 27, 'Salumificio', 'Culatelli'),
+        new Dipendente('IBIS', rand(100, 200), 'Ettore' , 'Gianfranco', 27, 'Salumificio', 'Culatelli'),
+
     ];
 
 } catch (Exception $e) {
