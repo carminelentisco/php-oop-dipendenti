8133c81772d2c482f3e4c4c0cecd1864c9d6c4ab
carminelentisco <carminelentisco@gmail.com>
2020-06-08 18:32:30 +0200

    Aggiunto bootstrap

--- index.php
+++ index.php
@@ -9,25 +9,36 @@ include_once __DIR__ . '/classes/informations.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Gestione Dipendenti</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
 <body>
     
+    <header>
+        <nav class="navbar navbar-light bg-light">
+            <span class="navbar-brand mb-0 h1">BoolDipendenti</span>
+        </nav>
+    </header>
         
-    <?php foreach ($informations as $info) { ?>
+    <main class="container d-flex justify-content-around flex-wrap">
+        <?php foreach ($informations as $info) { ?>
 
-    <ul class="dipendente-info">   
-        <li>Nome Sede: <?php echo $info->print('nomeSede') ?></li>
-        <li>ID Dipendente: <?php echo $info->print('dipendenteId') ?></li>
-        <li>Nome Dipendente: <?php echo $info->print('dipendenteNome') ?></li>
-        <li>Cognome Dipendente: <?php echo $info->print('dipendenteCognome') ?></li>
-        <li>Età Dipendente: <?php echo $info->print('dipendenteEta') ?></li>
-        <li>Reparto Dipendente: <?php echo $info->print('dipendenteReparto') ?></li>
-        <li>Postazione Dipendente: <?php echo $info->print('dipendentePostazione') ?></li>
-    </ul>
-    <?php } ?>
+        <ul class="list-group mt-4 mb-4 ">   
+            <li class="list-group-item">Nome Sede: <?php echo $info->print('nomeSede') ?></li>
+            <li class="list-group-item">ID Dipendente: <?php echo $info->print('dipendenteId') ?></li>
+            <li class="list-group-item">Nome Dipendente: <?php echo $info->print('dipendenteNome') ?></li>
+            <li class="list-group-item">Cognome Dipendente: <?php echo $info->print('dipendenteCognome') ?></li>
+            <li class="list-group-item">Età Dipendente: <?php echo $info->print('dipendenteEta') ?></li>
+            <li class="list-group-item">Reparto Dipendente: <?php echo $info->print('dipendenteReparto') ?></li>
+            <li class="list-group-item">Postazione Dipendente: <?php echo $info->print('dipendentePostazione') ?></li>
+        </ul>
+        <?php } ?>
+    </main>
 
-
+    <!-- Script -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 
 </body>
 </html>
